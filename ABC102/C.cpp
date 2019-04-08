3d14a4db642b409fb791f60d3be5e5375f54a9f6
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-04-08 09:30:09 +0900

    Submit_AC(give up)

--- ABC102/C.cpp
+++ ABC102/C.cpp
@@ -7,6 +7,17 @@ long funcABS( long x )
 	return ( x > 0 ? x : -x );
 }
 
+long calc_sad( const std::vector<long> &v, long b )
+{
+	long total = 0;
+	for ( int i = 0; i < v.size(); i++ )
+	{
+		total += funcABS( v[i] - ( b + ( i + 1 ) ) );
+	}
+
+	return ( total );
+}
+
 int main()
 {
 	unsigned int N;
@@ -25,33 +36,44 @@ int main()
 	// abs( A_i - i - b )
 
 	// A_i - i を先に計算する
-	long A_total = 0;
+	std::vector<long> vA;
 	for ( int i = 0; i < N; i++ )
 	{
-		A_total += ( v[i] - ( i + 1 ) );
+		vA.emplace_back( v[i] - ( i + 1 ) );
 	}
 
-	// A_total - N * bが一番小さくなるようなbを求める
-	// -> A_total / Nで良い？
-	long b = funcABS( A_total ) / N;
-	if ( A_total < 0 )
-	{
-		b *= -1;
-		if ( funcABS( A_total ) % N )
-		{
-			// 負の値で余りがある場合は切り捨て＝絶対値が1大きくなる
-			b -= 1;
-		}
-	}
+	// vAをソートする
+	std::sort( vA.begin(), vA.end(), []( long a, long b ) {
+			return ( a < b );
+		} );
+
+	// 中央値を取る
+	// 中央値は、Nが偶数の場合は2通り計算しておく
+	long b = vA[N / 2];
 
 	// 計算する
 	long sad_value = 0;
-	for ( int i = 0; i < N; i++ )
-	{
-		sad_value += funcABS( v[i] - ( b + ( i + 1 ) ) );
-	}
+	sad_value = calc_sad( v, b );
 
-	std::cout << sad_value << std::endl;
+	if ( ( N & 1 ) == 0 )	//	偶数の場合はこっちも計算する
+	{
+		long sad_value2 = 0;
+		long b_even = vA[N / 2 - 1];
+		sad_value2 = calc_sad( v, b_even );
+
+		if ( sad_value > sad_value2 )
+		{
+			std::cout << sad_value2 << std::endl;
+		}
+		else
+		{
+			std::cout << sad_value << std::endl;
+		}
+	}
+	else
+	{
+		std::cout << sad_value << std::endl;
+	}
 
 	return ( 0 );
 }
