0bffa210f35c2191498034babde9dfea6fb39cb0
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-04-18 16:27:50 +0900

    Submit_AC

--- ABC106/C.cpp
+++ ABC106/C.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
+	std::string S;
+	long long K;
+
+	std::cin >> S >> K;
+
+	// 1以外の文字は、N^5000兆文字増える
+	// -> Sを左から見ていって、初めて出てきた1以外の文字が、K番目の文字
+	char ans;
+	int i;
+	for ( i = 0; i < S.length(); i++ )
+	{
+		if ( S[i] != '1' )
+		{
+			ans = S[i];
+			break;
+		}
+	}
+
+	// ただし、Kがiより小さい場合は1になる
+	if ( K <= i )
+	{
+		std::cout << "1" << std::endl;
+	}
+	else
+	{
+		std::cout << ans << std::endl;
+	}
 
 	return ( 0 );
 }
