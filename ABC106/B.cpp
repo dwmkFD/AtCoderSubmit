c3cc7dce420daa71a5efcdc62c99f5653442829d
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-04-18 16:11:47 +0900

    Submit_AC

--- ABC106/B.cpp
+++ ABC106/B.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
+	int N;
+	std::cin >> N;
+
+	long counter;
+	std::vector<int> v;
+
+	v.emplace_back( 105 ); // 105以下で条件を満たすのは105だけ
+
+	// Nは最大200なので、ゴリ押し（全探索）
+	for ( int i = 107; i <= N; i += 2 )	//	107から探す
+	{
+		counter = 0;
+		for ( int j = 1; j <= N; j += 2 )	//	奇数でしか割れない
+		{
+			if ( i % j == 0 )
+			{
+				counter++;
+			}
+		}
+		if ( counter == 8 )
+		{
+			v.emplace_back( i );
+		}
+	}
+
+	if ( N < 105 )
+	{
+		std::cout << "0" << std::endl;
+	}
+	else
+	{
+		std::cout << v.size() << std::endl;
+	}
 
 	return ( 0 );
 }
