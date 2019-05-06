25854c12cc96cb0c4b1217c001e8d843d08ca25b
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-07 08:23:48 +0900

    Submit_AC(give up)

--- ABC108/C.cpp
+++ ABC108/C.cpp
@@ -2,6 +2,30 @@
 
 int main()
 {
+	long N, K;
+	std::cin >> N >> K;
+
+	long counter1 = 0, counter2 = 0;
+
+	for ( long i = 1; i <= N; i++ )
+	{
+		if ( ( i % K ) == 0 )
+		{
+			++counter1;
+		}
+		else if ( ( ( K & 1 ) == 0 )
+				  && ( ( i % K ) == ( K / 2 ) )
+			)
+		{
+			++counter2;
+		}
+	}
+
+	long result;
+	result = counter1 * counter1 * counter1;
+	result += counter2 * counter2 * counter2;
+
+	std::cout << result << std::endl;
 
 	return ( 0 );
 }
