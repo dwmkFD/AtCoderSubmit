73f4310d816d11ea0aa50580b4d76d5b25475092
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-07 11:34:35 +0900

    Submit_AC

--- ABC109/A.cpp
+++ ABC109/A.cpp
@@ -2,5 +2,17 @@
 
 int main()
 {
+	int A, B;
+	std::cin >> A >> B;
+
+	if ( ( A & 1 ) && ( B & 1 ) )
+	{
+		std::cout << "Yes" << std::endl;
+	}
+	else
+	{
+		std::cout << "No" << std::endl;
+	}
+
 	return ( 0 );
 }
