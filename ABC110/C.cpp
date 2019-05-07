b16179bf700090d5224305fccb8f441fb9d023f6
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-08 07:19:28 +0900

    Submit_AC

--- ABC110/C.cpp
+++ ABC110/C.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 int main()
 {
+	std::string S, T;
+	std::cin >> S >> T;
+
+	std::vector<int> sv( 26 ), tv( 26 );
+
+	for ( int i = 0; i < S.length(); i++ )
+	{
+		// S, Tのアルファベットの出現頻度を数える
+		sv[S[i] - 'a']++;
+		tv[T[i] - 'a']++;
+	}
+
+	std::sort( sv.begin(), sv.end() );
+	std::sort( tv.begin(), tv.end() );
+
+	bool isYes = true;
+	for ( int i = 0; i < sv.size(); i++ )
+	{
+		if ( sv[i] != tv[i] )
+		{
+			isYes = false;
+			break;
+		}
+	}
+
+	std::cout << ( isYes ? "Yes" : "No" ) << std::endl;
+
 	return ( 0 );
 }
