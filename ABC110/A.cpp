d22fbbe741c6b95e7556a773cfa1fd4fb7102c62
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-07 13:21:50 +0900

    Submit_AC

--- ABC110/A.cpp
+++ ABC110/A.cpp
@@ -2,5 +2,34 @@
 
 int main()
 {
+	int A, B, C;
+	std::cin >> A >> B >> C;
+
+	int ans;
+	if ( A > B )
+	{
+		if ( A > C )
+		{
+			ans = A * 10 + B + C;
+		}
+		else
+		{
+			ans = C * 10 + B + A;
+		}
+	}
+	else
+	{
+		if ( C > B )
+		{
+			ans = C * 10 + B + A;
+		}
+		else
+		{
+			ans = B * 10 + A + C;
+		}
+	}
+
+	std::cout << ans << std::endl;
+
 	return ( 0 );
 }
