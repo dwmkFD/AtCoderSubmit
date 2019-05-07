ba0098d993ccca35fa6671a55f7fd09724cd3cee
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-07 11:34:41 +0900

    Submit_AC

--- ABC109/B.cpp
+++ ABC109/B.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <string>
+#include <map>
 
 int main()
 {
+	int N;
+	std::cin >> N;
+
+	bool rule = true;
+	std::map<std::string, int> m;
+	char cBack = 0;
+
+	std::string tmp;
+	std::cin >> tmp;
+	m[tmp]++;
+	cBack = tmp.back();
+
+	for ( int i = 1; i < N; i++ )
+	{
+		std::cin >> tmp;
+
+		if ( ( m[tmp]++ != 0 ) || ( tmp[0] != cBack ) )
+		{
+			rule = false;
+			break;
+		}
+		else
+		{
+			cBack = tmp.back();
+		}
+	}
+
+	if ( rule )
+	{
+		std::cout << "Yes" << std::endl;
+	}
+	else
+	{
+		std::cout << "No" << std::endl;
+	}
+
 	return ( 0 );
 }
