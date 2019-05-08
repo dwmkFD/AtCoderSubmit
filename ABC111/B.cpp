4a3c78c5d049fdcbcec7fb1117c1fc6efc2333a0
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-08 15:11:35 +0900

    Submit_AC

--- ABC111/B.cpp
+++ ABC111/B.cpp
@@ -2,5 +2,21 @@
 
 int main()
 {
+	int N;
+	std::cin >> N;
+
+	const int table[] = {
+		111, 222, 333, 444, 555, 666, 777, 888, 999
+	};
+
+	for ( int i = 0; i < sizeof( table ) / sizeof( table[0] ); i++ )
+	{
+		if ( N <= table[i] )
+		{
+			std::cout << table[i] << std::endl;
+			break;
+		}
+	}
+
 	return ( 0 );
 }
