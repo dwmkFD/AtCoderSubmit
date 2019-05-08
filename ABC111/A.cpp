647f9966164d8e94718e871837c670d1c19fa6bd
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-08 15:11:26 +0900

    Submit_AC

--- ABC111/A.cpp
+++ ABC111/A.cpp
@@ -2,5 +2,32 @@
 
 int main()
 {
+	int n;
+	std::cin >> n;
+
+	int c = 100;
+	int result = 0;
+	for ( int i = 0; i < 3; i++ )
+	{
+		int tmp = n / c;
+		if ( tmp == 1 )
+		{
+			result += 9;
+		}
+		else if ( tmp == 9 )
+		{
+			result += 1;
+		}
+		else
+		{
+			result += tmp;
+		}
+		result *= 10;
+		n %= c;
+		c /= 10;
+	}
+
+	std::cout << result / 10 << std::endl;
+
 	return ( 0 );
 }
