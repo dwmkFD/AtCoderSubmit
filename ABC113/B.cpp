82e426e06372fbd0900766c9c6804b05745f046b
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-09 14:56:03 +0900

    Submit_AC

--- ABC113/B.cpp
+++ ABC113/B.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <vector>
+
+double abs_( double x )
+{
+	return ( x > 0 ? x : -x );
+}
 
 int main()
 {
+	int N, T, A;
+	std::cin >> N >> T >> A;
+
+	std::vector<double> v( N );
+	for ( int i = 0; i < N; i++ )
+	{
+		std::cin >> v[i];
+		v[i] = T - v[i] * 0.006;
+	}
+
+	double j = 0;
+	for ( int i = 1; i < N; i++ )
+	{
+		if ( abs_( v[i] - A ) < abs_( v[j] - A ) )
+		{
+			j = i;
+		}
+	}
+
+	std::cout << j + 1 << std::endl;
 
 	return ( 0 );
 }
