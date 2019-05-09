c0ea4e44123ed498c7e06d231dc090e540e10ab9
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-09 13:22:34 +0900

    Submit_AC

--- ABC112/A.cpp
+++ ABC112/A.cpp
@@ -2,6 +2,20 @@
 
 int main()
 {
+	int N;
+	std::cin >> N;
+
+	if ( N == 1 )
+	{
+		std::cout << "Hello World" << std::endl;
+	}
+	else
+	{
+		int A, B;
+		std::cin >> A >> B;
+
+		std::cout << A + B << std::endl;
+	}
 
 	return ( 0 );
 }
