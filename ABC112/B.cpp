f8a30490caeebf9e16d474b897b4c4c3c79918a4
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-09 13:22:38 +0900

    Submit_AC

--- ABC112/B.cpp
+++ ABC112/B.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
+	int N, T;
+	std::cin >> N >> T;
+
+	std::vector<std::pair<int, int> > v;
+	for ( int i = 0; i < N; i++ )
+	{
+		int c, t;
+		std::cin >> c >> t;
+		v.emplace_back( std::make_pair( c, t ) );
+	}
+
+	int cost = 1e5;
+	for ( auto &&it : v )
+	{
+		if ( it.second <= T )
+		{
+			if ( cost > it.first )
+			{
+				cost = it.first;
+			}
+		}
+	}
+
+	if ( cost == 1e5 )
+	{
+		std::cout << "TLE" << std::endl;
+	}
+	else
+	{
+		std::cout << cost << std::endl;
+	}
 
 	return ( 0 );
 }
