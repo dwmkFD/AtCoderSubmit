0e8acfaff9641a416b532bc8fbe6c0f848500e57
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-09 14:02:45 +0900

    Submit_AC(give up)

--- ABC112/D.cpp
+++ ABC112/D.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main()
 {
+	int N, M;
+	std::cin >> N >> M;
+
+	std::vector<int> v;
+	for ( int i = 1; i * i <= M; i++ )
+	{
+		if ( M % i != 0 )
+		{
+			continue;
+		}
+		else
+		{
+			v.emplace_back( i );
+			v.emplace_back( M / i );
+		}
+	}
+
+	std::sort( v.begin(), v.end() );
+
+	int d = 1;
+	for ( auto &&it : v )
+	{
+		if ( it > M / N )
+		{
+			break;
+		}
+
+		if ( d < it )
+		{
+			d = it;
+		}
+	}
+
+	std::cout << d << std::endl;
 
 	return ( 0 );
 }
