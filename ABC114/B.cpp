fcd2ee06877bdf6de07053fa5b89953bde9ebe4d
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-14 11:29:28 +0900

    Submit_AC

--- ABC114/B.cpp
+++ ABC114/B.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
+	std::string S;
+	std::cin >> S;
+
+	unsigned long long x = 0;
+	for ( int i = 0; i < S.size(); i++ )
+	{
+		x *= 10;
+		x += S[i] - '0';
+	}
+
+	unsigned int diff = 1e9;
+	for ( ; x > 0; x /= 10 )
+	{
+		int y = x % 1000;
+		int z = std::abs( 753 - y );
+
+		if ( z < diff )
+		{
+			diff = z;
+		}
+	}
+
+	std::cout << diff << std::endl;
 
 	return ( 0 );
 }
