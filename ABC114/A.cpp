15e539523e8c82c16f278567e645c2479f7ba6a6
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-14 11:29:25 +0900

    Submit_AC

--- ABC114/A.cpp
+++ ABC114/A.cpp
@@ -2,6 +2,17 @@
 
 int main()
 {
+	int X;
+	std::cin >> X;
+
+	switch ( X )
+	{
+	case 3: case 5: case 7:
+		std::cout << "YES" << std::endl;
+		break;
+	default:
+		std::cout << "NO" << std::endl;
+	}
 
 	return ( 0 );
 }
