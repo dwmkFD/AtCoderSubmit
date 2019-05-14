7315364d1bf3fadf9b7ae7c336b3069f44b22b98
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-14 11:50:12 +0900

    Submit_AC(give up)

--- ABC114/C.cpp
+++ ABC114/C.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+long long N;
+
+bool chk( std::string input )
+{
+	int cnt = 0;
+	char c[] = { '3', '5', '7' };
+
+	for ( int i = 0; i < 3; i++ )
+	{
+		cnt += ( input.find( c[i] ) != std::string::npos );
+	}
+
+	return ( cnt >= 3 );
+}
+
+int solv( std::string input )
+{
+	if ( std::stoll( input ) > N )
+	{
+		return ( 0 );
+	}
+	else
+	{
+		int ret = ( chk( input ) == true );
+		for ( auto &&it : {'3', '5', '7' } )
+		{
+			ret += solv( input + it );
+		}
+		return ( ret );
+	}
+}
 
 int main()
 {
+	std::cin >> N;
+
+	std::cout << solv( "0" ) << std::endl;
 
 	return ( 0 );
 }
