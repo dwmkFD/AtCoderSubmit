ca5cb9b7c478ed09597f08ed780892f6cd7a3fd5
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-15 10:37:00 +0900

    Submit_AC

--- ABC115/A.cpp
+++ ABC115/A.cpp
@@ -2,6 +2,29 @@
 
 int main()
 {
+	int D;
+	std::cin >> D;
+
+	switch ( D )
+	{
+	case 25:
+		std::cout << "Christmas" << std::endl;
+		break;
+
+	case 24:
+		std::cout << "Christmas Eve" << std::endl;
+		break;
+
+	case 23:
+		std::cout << "Christmas Eve Eve" << std::endl;
+		break;
+
+	case 22:
+		std::cout << "Christmas Eve Eve Eve" << std::endl;
+		break;
+	default:
+		break;
+	}
 
 	return ( 0 );
 }
