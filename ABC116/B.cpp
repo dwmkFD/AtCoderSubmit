22958ce8c003cef3ab558c7531b4b189ccb28e91
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-15 14:54:50 +0900

    Submit_AC

--- ABC116/B.cpp
+++ ABC116/B.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <map>
+
+int f( int n )
+{
+	if ( n % 2 )
+	{
+		return ( 3 * n + 1 );
+	}
+	else
+	{
+		return ( n / 2 );
+	}
+}
 
 int main()
 {
+	int s;
+	std::cin >> s;
+
+	std::map<int, int> m;
+	m[s]++;
+
+	int tmp = s;
+	long counter = 1;
+	while ( 1 )
+	{
+		tmp = f( tmp );
+		if ( m[tmp] != 0 )
+		{
+			std::cout << counter + 1 << std::endl;
+			break;
+		}
+		else
+		{
+			m[tmp]++;
+			counter++;
+		}
+	}
 
 	return ( 0 );
 }
