f26986dc5d0cf4f2428c1fb63fd344b13071aa4a
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-15 10:37:08 +0900

    Submit_AC

--- ABC115/C.cpp
+++ ABC115/C.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main()
 {
+	int N, K;
+	std::cin >> N >> K;
+
+	std::vector<int> v( N );
+	for ( int i = 0; i < N; i++ )
+	{
+		std::cin >> v[i];
+	}
+
+	std::sort( v.begin(), v.end() );
+
+	int diff = 1e9 + 1;
+	for ( int i = 0; i < N - K + 1; i++ )
+	{
+		if ( diff > ( v[i + K - 1] - v[i] ) )
+		{
+			diff = v[i + K - 1] - v[i];
+		}
+	}
+
+	std::cout << diff << std::endl;
 
 	return ( 0 );
 }
