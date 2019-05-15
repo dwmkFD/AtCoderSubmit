5982f9d5c99be8a675a79d0a1aeb612da85b788e
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-15 10:37:04 +0900

    Submit_AC

--- ABC115/B.cpp
+++ ABC115/B.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main()
 {
+	int N;
+	std::cin >> N;
+
+	std::vector<int> v( N );
+	for ( int i = 0; i < N; i++ )
+	{
+		std::cin >> v[i];
+	}
+
+	std::sort( v.begin(), v.end() );
+
+	int total = 0;
+	for ( int i = 0; i < N - 1; i++ )
+	{
+		total += v[i];
+	}
+
+	total += v[N - 1] / 2;
+
+	std::cout << total << std::endl;
 
 	return ( 0 );
 }
