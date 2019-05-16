cb5c1ed3fcfeeb0d9a2b9b7ee57e9dea22d0543f
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-16 11:11:22 +0900

    Submit_AC

--- ABC117/B.cpp
+++ ABC117/B.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main()
 {
+	int N;
+	std::cin >> N;
+
+	std::vector<int> v( N );
+	for ( int i = 0; i < N; i++ )
+	{
+		std::cin >> v[i];
+	}
+
+	std::sort( v.begin(), v.end(), []( int a, int b ) {
+			return( a > b );
+		} );
+
+	int total = 0;
+	for ( int i = 1; i < N; i++ )
+	{
+		total += v[i];
+	}
+
+	if ( total > v[0] )
+	{
+		std::cout << "Yes" << std::endl;
+	}
+	else
+	{
+		std::cout << "No" << std::endl;
+	}
 
 	return ( 0 );
 }
