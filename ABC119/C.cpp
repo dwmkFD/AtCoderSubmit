eaedf5d8e38f528f86a30a09cee54838004bb658
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-21 08:23:48 +0900

    Submit_AC(give up)

--- ABC119/C.cpp
+++ ABC119/C.cpp
@@ -32,11 +32,46 @@ using Pull = pair<ull, ull>;
 #define RALL( c ) ( c ).rbegin(), ( c ).rend()
 
 #define MOD ( 1e9 + 7 )
+#define INF ( 1e9 )
+#define MINF ( -1e9 )
 
+int N, A, B, C;
+vector<int> v;
+
+int solv( int n, int a, int b, int c )
+{
+	if ( n == N )
+	{
+		if ( min( { a, b, c } ) > 0 )
+		{
+			return ( abs( A - a ) + abs( B - b ) + abs( C - c ) - 30 );
+		}
+		else
+		{
+			return ( INF );
+		}
+	}
+
+	int r0 = solv( n + 1, a, b, c );
+	int r1 = solv( n + 1, a + v[n], b, c ) + 10;
+	int r2 = solv( n + 1, a, b + v[n], c ) + 10;
+	int r3 = solv( n + 1, a, b, c + v[n] ) + 10;
+
+	return ( min( { r0, r1, r2, r3 } ) );
+}
 
 int main()
 {
+	cin >> N >> A >> B >> C;
 
+	rep( i, N )
+	{
+		int tmp;
+		cin >> tmp;
+		v.eb( tmp );
+	}
+
+	cout << solv( 0, 0, 0, 0 ) << endl;
 
 	return ( 0 );
 }
