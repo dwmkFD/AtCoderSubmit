bfea03bcf69ed6e6a88522395340ddecf9aaef6a
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-21 09:10:01 +0900

    Submit_AC(give up)

--- ABC119/D.cpp
+++ ABC119/D.cpp
@@ -33,10 +33,76 @@ using Pull = pair<ull, ull>;
 
 #define MOD ( 1e9 + 7 )
 
+#define INF  ( 1e12 )
+#define MINF ( -1e12 )
 
 int main()
 {
+	int A, B, Q;
+	cin >> A >> B >> Q;
 
+	vector<ll> vS( A + 2 );
+	rep( i, A )
+	{
+		cin >> vS[i];
+	}
+
+	vector<ll> vT( B + 2 );
+	rep( i, B )
+	{
+		cin >> vT[i];
+	}
+
+	vector<ll> vX( Q );
+	rep( i, Q )
+	{
+		cin >> vX[i];
+	}
+
+	// vS, vT に正負の無限遠の点を入れておく
+	vS[A] = MINF; vS[A + 1] = INF;
+	vT[B] = MINF; vT[B + 1] = INF;
+
+	sort( ALL( vS ) ); sort( ALL( vT ) );
+
+	for ( auto &&it : vX )
+	{
+		// 問itに対して、一番近い左右の寺2社2を求める
+		auto AB = upper_bound( ALL( vS ), it );
+		ll sB = *AB;
+		ll sA = *prev( AB );
+
+		auto CD = upper_bound( ALL( vT ), it );
+		ll tD = *CD;
+		ll tC = *prev( CD );
+
+		/* 上記4箇所の訪れ方の最小値を求める */
+		// 1. A -> C
+		ll ac = abs( sA - it ) + abs( tC - sA );
+
+		// 2. A -> D
+		ll ad = abs( sA - it ) + abs( tD - sA );
+
+		// 3. C -> A
+		ll ca = abs( tC - it ) + abs( sA - tC );
+
+		// 4. D -> A
+		ll da = abs( tD - it ) + abs( sA - tD );
+
+		// 5. B -> C
+		ll bc = abs( sB - it ) + abs( tC - sB );
+
+		// 6. B -> D
+		ll bd = abs( sB - it ) + abs( tD - sB );
+
+		// 7. C -> B
+		ll cb = abs( tC - it ) + abs( sB - tC );
+
+		// 8. D -> B
+		ll db = abs( tD - it ) + abs( sB - tD );
+
+		cout << min( { ac, ad, ca, da, bc, bd, cb, db } ) << endl;
+	}
 
 	return ( 0 );
 }
