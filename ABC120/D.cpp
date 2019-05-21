c4d5771fb8d82df60f148183a5af6d163597568b
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-21 13:49:18 +0900

    Submit_AC(give up)

--- ABC120/D.cpp
+++ ABC120/D.cpp
@@ -15,11 +15,14 @@ using namespace std;
 using ll = long long;
 using ull = unsigned long long;
 
+using Pint = pair<int, int>;
 using Pll  = pair<ll, ll>;
 using Pull = pair<ull, ull>;
 
 #define eb emplace_back
 #define pb push_back
+#define F first
+#define S second
 #define mp( a, b ) make_pair( a, b )
 #define mt( ... ) make_tuple( __VA_ARGS__ )
 
@@ -33,10 +36,94 @@ using Pull = pair<ull, ull>;
 
 #define MOD ( 1e9 + 7 )
 
+class UnionFind {
+public:
+	UnionFind( ll n ) : par( n ), siz( n, 1LL )
+	{
+		rep( i, n )
+		{
+			par[i] = i;
+		}
+	}
+
+	ll find( ll x )
+	{
+		if ( par[x] == x )
+		{
+			return ( x );
+		}
+		else
+		{
+			return ( par[x] = find( par[x] ) );
+		}
+	}
+
+	ll size( ll x )
+	{
+		return ( siz[find( x )] );
+	}
+
+	void unite( ll x, ll y )
+	{
+		x = find( x );
+		y = find( y );
+
+		if ( x == y ) return;
+
+		if ( siz[x] < siz[y] )
+		{
+			par[x] = y;
+			siz[y] += siz[x];
+		}
+		else
+		{
+			par[y] = x;
+			siz[x] += siz[y];
+		}
+	}
+
+private:
+	vector<ll> par;
+	vector<ll> siz;
+};
 
 int main()
 {
+	ll N, M;
+	cin >> N >> M;
 
+	vector<Pll> v;
+	rep( i, M )
+	{
+		ll a, b;
+		cin >> a >> b;
+		v.eb( mp( a - 1, b - 1 ) );
+	}
+
+	UnionFind uf( N );
+	vector<ll> ans( M, 0 );
+	ans[M - 1] = ( N * ( N - 1 ) ) / 2;
+
+	for ( int i = M - 1; i >= 1; --i )
+	{
+		ll n1 = uf.size( v[i].F );
+		ll n2 = uf.size( v[i].S );
+
+		if ( uf.find( v[i].F ) == uf.find( v[i].S ) )
+		{
+			ans[i - 1] = ans[i];
+		}
+		else
+		{
+			ans[i - 1] = ans[i] - n1 * n2;
+			uf.unite( v[i].F, v[i].S );
+		}
+	}
+
+	rep( i, M )
+	{
+		cout << ans[i] << endl;
+	}
 
 	return ( 0 );
 }
