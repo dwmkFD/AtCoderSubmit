6f0259625be54afaa05862e1c529e566876c98c8
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-23 13:10:43 +0900

    Submit_AC(give up)

--- ABC122/C.cpp
+++ ABC122/C.cpp
@@ -23,6 +23,9 @@ using Pull = pair<ull, ull>;
 #define mp( a, b ) make_pair( a, b )
 #define mt( ... ) make_tuple( __VA_ARGS__ )
 
+#define F first
+#define S second
+
 #define rep( i, n ) for ( int i = 0; i < (int)( n ); ++i )
 #define reps( i, n ) for ( int i = 1; i <= (int)( n ); ++i )
 #define rrep( i, n ) for ( int i = (int)( ( n ) - 1 ); i >= 0; --i )
@@ -36,7 +39,35 @@ using Pull = pair<ull, ull>;
 
 int main()
 {
+	ll N, Q;
+	string S;
+	cin >> N >> Q;
+	cin >> S;
 
+	vector<Pll> v( Q );
+	rep( i, Q )
+	{
+		cin >> v[i].F >> v[i].S;
+	}
+
+	vector<ll> vv( N + 1 );
+	vv[0] = 0;
+	reps( i, N - 1 )
+	{
+		vv[i] = vv[i - 1];
+		if ( S[i] == 'C' )
+		{
+			if ( S[i - 1] == 'A' )
+			{
+				vv[i] += 1;
+			}
+		}
+	}
+
+	rep( i, Q )
+	{
+		cout << ( vv[v[i].S - 1] - vv[v[i].F - 1] ) << endl;
+	}
 
 	return ( 0 );
 }
