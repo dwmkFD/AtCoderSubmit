e6cf30be89dfba8fdd5e25dd10c225695106fb16
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-24 14:58:16 +0900

    Submit_AC(give up)

--- ABC122/D.cpp
+++ ABC122/D.cpp
@@ -31,12 +31,62 @@ using Pull = pair<ull, ull>;
 #define ALL( c ) ( c ).begin(), ( c ).end()
 #define RALL( c ) ( c ).rbegin(), ( c ).rend()
 
-#define MOD ( 1e9 + 7 )
+#define MOD ( (ll)( 1e9 + 7 ) )
 
+ll N;
+vector<map<string, ll>> dp;
+
+bool chk( const string &acgt )
+{
+	rep( i, 4 )
+	{
+		string tmp = acgt;
+		if ( i >= 1 )
+		{
+			swap( tmp[i], tmp[i - 1] );
+		}
+
+		if ( tmp.find( "AGC" ) != string::npos )
+		{
+			return ( false );
+		}
+	}
+
+	return ( true );
+}
+
+ll dfs( ll current, const string str3 )
+{
+	if ( dp[current][str3] != 0 )
+	{
+		return ( dp[current][str3] );
+	}
+
+	if ( current == N )
+	{
+		return ( 1 );
+	}
+
+	ll ret = 0;
+	const string acgt = "ACGT";
+	rep( i, 4 )
+	{
+		if ( chk( str3 + acgt[i] ) )
+		{
+			ret = ( ret + dfs( current + 1, str3.substr( 1, 2 ) + acgt[i] ) ) % MOD;
+		}
+	}
+	dp[current][str3] = ret;
+
+	return ( ret );
+}
 
 int main()
 {
+	cin >> N;
+	dp.resize( N + 1 );
 
+	cout << dfs( 0, "TTT" ) << endl;
 
 	return ( 0 );
 }
