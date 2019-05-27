f0b96b6914a01d537315fdf82d00c035675bb82b
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-27 11:15:30 +0900

    Submit_AC(give up)

--- ABC123/D.cpp
+++ ABC123/D.cpp
@@ -1,37 +1,84 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <string>
+#include <bitset>
+#include <tuple>
+#include <cmath>
+#include <map>
+
+template<typename T> bool chmax( T &a, T b ) { if ( a < b ) { a = b; return ( true ); } else { return ( false ); } }
+template<typename T> bool chmin( T &a, T b ) { if ( a > b ) { a = b; return ( true ); } else { return ( false ); } }
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+
+using Pll  = pair<ll, ll>;
+using Pull = pair<ull, ull>;
+
+#define eb emplace_back
+#define pb push_back
+#define mp( a, b ) make_pair( a, b )
+#define mt( ... ) make_tuple( __VA_ARGS__ )
+
+#define F first
+#define S second
+
+#define rep( i, n ) for ( int i = 0; i < (int)( n ); ++i )
+#define reps( i, n ) for ( int i = 1; i <= (int)( n ); ++i )
+#define rrep( i, n ) for ( int i = (int)( ( n ) - 1 ); i >= 0; --i )
+#define rreps( i, n ) for ( int i = (int)( ( n ) ); i > 0; --i )
+
+#define ALL( c ) ( c ).begin(), ( c ).end()
+#define RALL( c ) ( c ).rbegin(), ( c ).rend()
+
+#define MOD ( (ll)( 1e9 + 7 ) )
+
 
 int main()
 {
-	unsigned long long X, Y, Z, K;
-	std::cin >> X >> Y >> Z >> K;
+	ll X, Y, Z, K;
+	cin >> X >> Y >> Z >> K;
 
-	std::vector<unsigned long long> A;
-	std::vector<unsigned long long> B;
-	std::vector<unsigned long long> C;
+	vector<ll> A( X ), B( Y ), C( Z );
+	rep( i, X ) { cin >> A[i]; }
+	rep( i, Y ) { cin >> B[i]; }
+	rep( i, Z ) { cin >> C[i]; }
 
-	for ( int i = 0; i < X; i++ )
+	// A/B/Cを降順ソート
+	sort( ALL( A ), greater<ll>() );
+	sort( ALL( B ), greater<ll>() );
+	sort( ALL( C ), greater<ll>() );
+
+	// A/B/Cからそれぞれi/j/k番目のケーキを選ぶパターンを
+	// i * j * k <= K となるように全て選び、最後にソートする
+	vector<ll> res;
+	rep( i, X )
 	{
-		unsigned long long tmp;
-		std::cin >> tmp;
-		A.emplace_back( tmp );
+		rep( j, Y )
+		{
+			rep( k, Z )
+			{
+				if ( ( i + 1 ) * ( j + 1 ) * ( k + 1 ) <= K )
+				{
+					res.eb( A[i] + B[j] + C[k] );
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
 	}
 
-	for ( int i = 0; i < Y; i++ )
+	sort( ALL( res ), greater<ll>() );
+
+	rep( i, K )
 	{
-		unsigned long long tmp;
-		std::cin >> tmp;
-		B.emplace_back( tmp );
+		cout << res[i] << endl;
 	}
-
-	for ( int i = 0; i < Z; i++ )
-	{
-		unsigned long long tmp;
-		std::cin >> tmp;
-		C.emplace_back( tmp );
-	}
-
-
 
 	return ( 0 );
 }
