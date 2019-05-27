a2130603a7d3459977c9d4f330b244987ae37874
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-27 11:16:28 +0900

    first commit ABC124

---
+++ ABC124/A.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bitset>
+#include <tuple>
+#include <cmath>
+#include <map>
+
+template<typename T> bool chmax( T &a, T b ) { if ( a < b ) { a = b; return ( true ); } else { return ( false ); } }
+template<typename T> bool chmin( T &a, T b ) { if ( a > b ) { a = b; return ( true ); } else { return ( false ); } }
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+
+using Pll  = pair<ll, ll>;
+using Pull = pair<ull, ull>;
+
+#define eb emplace_back
+#define pb push_back
+#define mp( a, b ) make_pair( a, b )
+#define mt( ... ) make_tuple( __VA_ARGS__ )
+
+#define F first
+#define S second
+
+#define rep( i, n ) for ( int i = 0; i < (int)( n ); ++i )
+#define reps( i, n ) for ( int i = 1; i <= (int)( n ); ++i )
+#define rrep( i, n ) for ( int i = (int)( ( n ) - 1 ); i >= 0; --i )
+#define rreps( i, n ) for ( int i = (int)( ( n ) ); i > 0; --i )
+
+#define ALL( c ) ( c ).begin(), ( c ).end()
+#define RALL( c ) ( c ).rbegin(), ( c ).rend()
+
+#define MOD ( (ll)( 1e9 + 7 ) )
+
+
+int main()
+{
+
+
+	return ( 0 );
+}
