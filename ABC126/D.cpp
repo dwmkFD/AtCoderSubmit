f36478e4f8a2080a5b19390829017e174891214e
U-MELCO\LZ70214 <LZ70214@meiw0983.ad.melco.co.jp>
2019-05-30 08:02:11 +0900

    Submit_AC(give up)

--- ABC126/D.cpp
+++ ABC126/D.cpp
@@ -5,8 +5,8 @@
 #include <tuple>
 #include <bitset>
 
-template<typename T> bool chmax( T &a, T b ) { if ( a > b ) { a = b; return ( true ); } else { return ( false ); } }
-template<typename T> bool chmin( T &a, T b ) { if ( a < b ) { a = b; return ( true ); } else { return ( false ); } }
+template<typename T> bool chmax( T &a, T b ) { if ( a <= b ) { a = b; return ( true ); } else { return ( false ); } }
+template<typename T> bool chmin( T &a, T b ) { if ( a >= b ) { a = b; return ( true ); } else { return ( false ); } }
 
 using namespace std;
 
@@ -16,9 +16,13 @@ using ull = unsigned long long;
 using Pll  = pair<ll, ll>;
 using Pull = pair<ull, ull>;
 
-#define ep emplace_back
+#define eb emplace_back
 #define pb push_back
 #define mp( a, b ) make_pair( a, b )
+#define mt make_tuple
+
+#define F first
+#define S second
 
 #define rep( i, n ) for ( int i = 0; i < (int)( n ); ++i )
 #define reps( i, n ) for ( int i = 1; i <= (int)( n ); ++i )
@@ -30,10 +34,62 @@ using Pull = pair<ull, ull>;
 
 #define MOD ( 1e9 + 7 )
 
+using Edge = pair<int, int>;
+using Graph = vector<vector<Edge>>;
+
+Graph G;
+vector<int> res;
+
+void dfs( int v, int p, int c )
+{
+	res[v] = c;	//	ノードvはcで塗る
+
+	for ( auto &&e : G[v] )	//	ノードvに繋がっているノード全部
+	{
+		if ( e.F == p )
+		{
+			// 親は考慮不要
+			continue;
+		}
+		if ( e.S % 2 )
+		{
+			// 繋がってるノードへの距離が奇数
+			dfs( e.F, v, c ^ 1 );	//	子は違う色にする
+		}
+		else
+		{
+			dfs( e.F, v, c );	//	偶数なので同じ色
+		}
+	}
+}
 
 int main()
 {
+	ll N;
+	cin >> N;
 
+	G.assign( N, vector<Edge>() );
+	res.assign( N, 0 );
+
+	rep( i, N - 1 )
+	{
+		int u, v, w;
+		cin >> u >> v >> w;
+
+		--u; --v;
+
+		G[u].emplace_back( Edge( v, w ) );
+		G[v].emplace_back( Edge( u, w ) );
+	}
+
+	// 計算
+	dfs( 0, -1, 0 );
+
+	// 出力
+	for ( auto &&it : res )
+	{
+		cout << it << endl;
+	}
 
 	return ( 0 );
 }
