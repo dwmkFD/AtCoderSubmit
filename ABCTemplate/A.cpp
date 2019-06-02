275ab62bb9bc7fecc83113cf9e406d0321160546
FD（えふでぃ） <fd.dwmk@gmail.com>
2019-06-03 00:05:00 +0900

    ABCTemplate Update

--- ABCTemplate/A.cpp
+++ ABCTemplate/A.cpp
@@ -65,6 +65,14 @@ template<typename T = ll> T binpow( T a, T b, T m = MOD ) {
 	return ( res );
 }
 
+void replace( string &s, string t, string r ) {
+	string::size_type p = 0;
+	while ( ( p = s.find( t, p ) ) != string::npos ) {
+		s.replace( p, t.length(), r );
+		p += r.length();
+	}
+}
+
 
 int main()
 {
