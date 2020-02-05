d0ec9497d2dd77757e9e33f06697334072dab6c2
EX80450 <inaba.takafumi@bc.mitsubishielectric.co.jp>
2020-02-05 13:06:45 +0900

    ABC Template Update

--- ABCTemplate/A.cpp
+++ ABCTemplate/A.cpp
@@ -45,7 +45,6 @@ template<typename T> T lcm( const T a, const T b ) { return ( a / gcd( a, b ) * 
 #define RALL( c ) ( c ).rbegin(), ( c ).rend()
 #define UNIQUE( c ) ( c ).erase( unique( ( c ).begin(), ( c ).end() ), ( c ).end() )
 
-constexpr ll MOD = 1000000007LL;
 template<typename T = ll> constexpr T MAX = numeric_limits<T>::max();
 
 #define y0 y3487465
@@ -96,12 +95,73 @@ template<typename T = ll> T solveLIS( const vector<T> &v ) {
 	return ( distance( dp.begin(), lower_bound( ALL( dp ), numeric_limits<T>::max() ) ) );
 }
 
-template<typename T = ll> T power( T a, T b, T m = MOD ) {
-	T res = 1;
-	while ( b > 0 ) { if ( b & 1 ) res = res * a % m;
-		a = a * a % m; b >>= 1; }
-	return ( res );
-}
+template<typename T, T MOD> class Fp {
+public:
+	constexpr Fp( T input ) noexcept : val( input % MOD ) {
+		if ( val < 0 ) val += MOD;
+	}
+	constexpr T getmod() { return ( MOD ); }
+	constexpr Fp operator-() const noexcept {
+		return ( val ? MOD - val : 0 );
+	}
+	constexpr Fp operator+( const Fp &r ) const noexcept { return ( ( Fp( *this ) += r ) ); }
+	constexpr Fp operator-( const Fp &r ) const noexcept { return ( ( Fp( *this ) -= r ) ); }
+	constexpr Fp operator*( const Fp &r ) const noexcept { return ( ( Fp( *this ) *= r ) ); }
+	constexpr Fp operator/( const Fp &r ) const noexcept { return ( ( Fp( *this ) /= r ) ); }
+
+	constexpr Fp &operator+=( const Fp &r ) noexcept {
+		val += r.val;
+		if ( val >= MOD ) val -= MOD;
+		return ( *this );
+	}
+
+	constexpr Fp &operator-=( const Fp &r ) noexcept {
+		val -= r.val;
+		if ( val < 0 ) val += MOD;
+		return ( *this );
+	}
+
+	constexpr Fp &operator*=( const Fp &r ) noexcept {
+		val = ( val * r.val ) % MOD;
+		return ( *this );
+	}
+
+	constexpr Fp &operator/=( const Fp &r ) noexcept {
+		T a = r.val, b = MOD, u = 1, v = 0;
+		while ( b ) {
+			T t = a / b;
+			a -= t * b; swap( a, b );
+			u -= t * v; swap( u, v );
+		}
+		val = ( val * u ) % MOD;
+		if ( val < 0 ) val += MOD;
+		return ( *this );
+	}
+
+	constexpr bool operator==( const Fp &r ) noexcept {
+		return ( val == r.val );
+	}
+
+	constexpr bool operator!=( const Fp &r ) noexcept {
+		return ( val != r.val );
+	}
+
+	friend constexpr ostream& operator<<( ostream &os, const Fp<T, MOD>&x ) noexcept {
+		return ( os << x.val );
+	}
+
+	friend constexpr Fp<T, MOD> modpow( const Fp<T, MOD> &a, T n ) noexcept {
+		if ( n == 0 ) return ( 1 );
+		auto t = modpow( a, n / 2 );
+		t = t * t;
+		if ( n & 1 ) t = t * a;
+        return ( t );
+	}
+private:
+	T val;
+};
+constexpr ll MOD = 1000000007LL;
+using mint = Fp<ll, MOD>;
 
 /*
 constexpr ll COMBSIZE = 500010;
