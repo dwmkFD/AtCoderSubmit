b8d323a3fa1f3f6039813d6b5a9f8eaee1369bd2
FD（えふでぃ） <fd.dwmk@gmail.com>
2020-05-09 01:53:37 +0900

    ABCTemplate modify

--- ABCTemplate/A.cpp
+++ ABCTemplate/A.cpp
@@ -77,32 +77,49 @@ template<typename T = ll> class CompareMin {
 public:	T operator()( T a, T b ) { return ( min( a, b ) ); }
 };
 
-template<typename T = ll, typename F = CompareMax<T>, T I = 0> struct SegTree {
-	T N; F func; vector<T> v;
+template<typename Monoid, typename T = ll> struct SegTree {
+	using Func = function<Monoid( Monoid, Monoid )>;
+	const Func func;
+	const Monoid UNITY;
+	T SIZE_R;
+	vector<Monoid> data;
 
-	SegTree( T n ) {
-		N = n; v.resize( 4 * n );
-		rep( i, 4 * n ) v[i] = I;
-	}
-
-	void update( T i, T x ) {
-		i += N - 1;
-		v[i] = x;
-		while ( i > 0 ) {
-			i = ( i - 1 ) / 2;
-			v[i] = func( v[i * 2 + 1], v[i * 2 + 2] );
+	SegTree( T n, const Func f, const Monoid &unity ) :
+		func( f ), UNITY( unity ) {
+			init( n );
 		}
+
+	void init( T n ) {
+		SIZE_R = 1;
+		while ( SIZE_R < n ) SIZE_R *= 2;
+		data.assign( SIZE_R * 2, UNITY );
 	}
 
-	T query( T a, T b, T k, T l, T r ) {
-		if ( r <= a || b <= l ) return ( I );
-		if ( a <= l && r <= b ) return ( v[k] );
-		else {
-			T vl = query( a, b, k * 2 + 1, l, ( l + r ) / 2 );
-			T vr = query( a, b, k * 2 + 2, ( l + r ) / 2, r );
-			return ( func( vl, vr ) );
-		}
+	void set( T idx, const Monoid &val ) { data[idx + SIZE_R] = val; }
+	void build() {
+		for ( T k = SIZE_R - 1; k > 0; --k )
+			data[k] = func( data[k * 2], data[k * 2 + 1] );
 	}
+
+	void update( T idx, const Monoid &val ) {
+		T k = idx + SIZE_R;
+		data[k] = val;
+		while ( k >>= 1 ) data[k] = func( data[k * 2], data[k * 2 + 1] );
+	}
+
+	Monoid get( T a, T b ) {
+		Monoid vleft = UNITY, vright = UNITY;
+		for ( T left = a + SIZE_R, right = b + SIZE_R;
+			  left < right;
+			  left >>= 1, right >>= 1 ) {
+			if ( left & 1 ) vleft = func( vleft, data[left++] );
+			if ( right & 1 ) vright = func( data[--right], vright );
+		}
+
+		return ( func( vleft, vright ) );
+	}
+
+	inline Monoid operator[]( ll idx ) { return ( data[idx + SIZE_R] ); }
 };
 
 template<typename T = ll> T solveLIS( const vector<T> &v ) {
