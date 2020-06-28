f48570b2ef90d3cfc8a1ffa36e1007ba30e9e26b
FD（えふでぃ） <fd.dwmk@gmail.com>
2020-06-29 08:38:44 +0900

    ABCTemplate modify

--- ABCTemplate/A.cpp
+++ ABCTemplate/A.cpp
@@ -280,6 +280,20 @@ void replace( string &s, string t, string r ) {
 	}
 }
 
+template<typename T> auto binsearch_idx( vector<T> &v, T val ) {
+	ll left = -1, right = v.size();
+	while ( abs( right - left ) > 1 )
+	{
+		ll mid = ( left + right ) / 2;
+		if ( val > v[mid] )
+			left = mid;
+		else
+			right = mid;
+	}
+
+	return ( mp( v[right] == val, right ) );
+}
+
 
 int main()
 {
