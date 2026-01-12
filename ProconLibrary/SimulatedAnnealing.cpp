cfda8b103a744088739760714ca9d40ff82bfd22
FD（えふでぃ） <fd.dwmk@gmail.com>
2026-01-12 10:14:17 +0900

    SA update

--- ProconLibrary/SimulatedAnnealing.cpp
+++ ProconLibrary/SimulatedAnnealing.cpp
@@ -16,19 +16,20 @@ template<typename T> T calc_score( State &state ) {
 void solve_sa() {
 	State state;
 	init( state );
-	double start_temp, end_temp;
-	double start_time;
+	double start_temp = 50.0, end_temp = 10.0;
+	auto start_time = chrono::steady_clock::now();
 
 	while ( true ) {
-		double now_time;
-		if ( now_time - start_time > TIME_LIMIT ) break;
+		auto now_time = chrono::steady_clock::now();
+		double elapse = chrono::duration<double>( now_time - start_time ).count();
+		if ( elapse > TIME_LIMIT ) break;
 
 		State new_state = state;
 		modify( new_state );
 		ll new_score = calc_score<ll>( new_state );
 		ll pre_score = calc_score<ll>( state );
 
-		double temp = start_temp + ( end_temp - start_temp ) * ( now_time - start_time ) / TIME_LIMIT;
+		double temp = start_temp + ( end_temp - start_temp ) * elapse / TIME_LIMIT;
 		double prob = exp( ( new_score - pre_score ) / temp );
 
 		if ( prob > rand() / (double)INF ) {
